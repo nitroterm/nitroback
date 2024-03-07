4fb44ed91a460271883aaf019d2c93dc0dbac786
Cacahuète Dev <24205659+CacahueteSansSel@users.noreply.github.com>
2024-03-07 09:05:38 +0100

    initial commit

---
+++ Nitroterm.Backend/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nitroterm.Backend.Controllers;
+
+[ApiController]
+[Route("api/test")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello, World !";
+    }
+}
